00f6bfbe280b509fa191e661da3eb78fc11828e1
hibow <utfnalin@gmail.com>
2019-05-25 01:10:37 -0500

    edit eslint for react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,13 @@ module.exports = {
   "globals": {
       "document": "readonly"
   },
+  "plugins": [
+    "react-hooks"
+  ],
   "rules": {
       // windows linebreaks when not in production environment
-      "linebreak-style": 0
+      "linebreak-style": 0,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
   }
 };
