195f73b4116f0677529895ffb5ee62b45afa0bb8
hibow <utfnalin@gmail.com>
2019-05-28 17:50:39 -0500

    add form stylesheet

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import Submit from '../Button/Submit';
-// import Inputleft from './Inputleft';
-// import Inputright from './Inputright';
+import Inputfind from './Inputfind';
+import Inputnear from './Inputnear';
 // TODO: combine two search inputs
 // Todo: alignment for search bar
 // todo: useraccount drop down
 const Searchbar = () => (
   <div className="main-search-wrap grid_unit grid_unit--fill align-middle">
-    <form className="main-search yform u-space-b20">
+    <form className="main-search l-form u-space-b20">
       <div className="grid">
         <div className="grid_unit grid_unit--fill">
           <div className="grid grid--equal">
-            this is input one
-            this is input two
-            {/* <Inputleft />
-            <Inputright /> */}
+            <Inputfind />
+            <Inputnear />
           </div>
         </div>
         <div className="grid_unit main-search_action">
