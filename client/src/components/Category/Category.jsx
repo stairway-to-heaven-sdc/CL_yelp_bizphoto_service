d407bb4cd02a65fdfcbfc2be62ba1305c2c22e83
hibow <utfnalin@gmail.com>
2019-05-28 00:29:06 -0500

    add category and useraccountbtn

---
+++ client/src/components/Category/Category.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Category = ({ price, category }) => (
+  <div className="price-category">
+    <span className="bullet-after">
+      <span className="biz-data price-range">{price}</span>
+    </span>
+    <span className="category-list">
+      {` ${category.join(', ')}`}
+    </span>
+    <a
+      href="https://www.yelp.com"
+      className="edit-business-button chiclet-link chiclet-link--with-text"
+    >
+    Edit
+    </a>
+  </div>
+);
+
+export default Category;
