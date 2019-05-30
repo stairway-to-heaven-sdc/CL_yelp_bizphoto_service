58868edc36bbc0b365defdbb21c72fee170a21cc
hibow <utfnalin@gmail.com>
2019-05-30 00:52:25 -0500

    add useIndex test

---
+++ client/src/lib/__test__/useIndex.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import useIndex from '../useIndex';
+
+const HookWrapper = (props) =>{
+  const hook = props.hook ? props.hook() : undefined;
+  return <div hook={hook} />;
+}
+describe('useIndex', () => {
+  it('should render', () => {
+    let wrapper = mount(<HookWrapper />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+  it('should set init value', ()=> {
+    let wrapper = shallow(<HookWrapper hook={() => useIndex(0, 3)}/>);
+    let {hook} = wrapper.find('div').props();
+    let {index, handleleft, handleright} = hook;
+    expect(index).toEqual(0);
+    wrapper = shallow(<HookWrapper hook={() => useIndex(2, 5)}/>);
+    ({ hook } = wrapper.find('div').props());
+    ({index, handleleft, handleright} = hook);
+    expect(index).toEqual(2);
+  });
+  it ('should change index when handleright is called', () => {
+    let wrapper = shallow(<HookWrapper hook={() => useIndex(0, 3)}/>);
+    let {hook} = wrapper.find('div').props();
+    let {index, handleleft, handleright} = hook;
+    expect(index).toEqual(0);
+    handleright();
+    ({ hook } = wrapper.find('div').props());
+    ({index, handleleft, handleright} = hook);
+    expect(index).toEqual(1);
+  });
+  it ('should change index when handleleft is called', () => {
+    let wrapper = shallow(<HookWrapper hook={() => useIndex(1, 3)}/>);
+    let {hook} = wrapper.find('div').props();
+    let {index, handleleft, handleright} = hook;
+    expect(index).toEqual(1);
+    handleleft();
+    ({ hook } = wrapper.find('div').props());
+    ({index, handleleft, handleright} = hook);
+    expect(index).toEqual(0);
+  });
+  it ('should not decrease index when index is 0', () => {
+    let wrapper = shallow(<HookWrapper hook={() => useIndex(0, 3)}/>);
+    let {hook} = wrapper.find('div').props();
+    let {index, handleleft, handleright} = hook;
+    expect(index).toEqual(0);
+    handleleft();
+    ({ hook } = wrapper.find('div').props());
+    ({index, handleleft, handleright} = hook);
+    expect(index).toEqual(0);
+  });
+})
---
+++ webpack.config.prod.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'production',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'biz_bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/client/dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // /\.jsx$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          publicPath: DIST_DIR,
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new SpriteLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
+};
