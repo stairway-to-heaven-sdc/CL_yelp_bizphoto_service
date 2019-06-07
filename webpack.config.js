b4884a847bf2e17d388d568154d53f4fc4d371ab
hibow <utfnalin@gmail.com>
2019-06-07 14:41:50 -0500

    split vendor file

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,19 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        vendor: {
+          name: 'vendor',
+          chunks: 'all',
+          test: /node_modules/,
+        },
+      },
+    },
+  },
   plugins: [
     new SpriteLoaderPlugin({
       plainSprite: true,
