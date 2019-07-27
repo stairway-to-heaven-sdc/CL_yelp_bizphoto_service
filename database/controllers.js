05988831a6ebc0bf0342e6ffebb78c9b001965eb
Bruce <bruce@yogainternational.com>
2019-07-27 19:14:21 -0400

    recursive attempt at seeding script

--- database/controllers.js
+++ database/controllers.js
@@ -1,5 +1,10 @@
-const createEntry = image => {
+/* eslint-disable no-console */
+const { Photo } = require('../database/index');
+
+const createEntry = (image, cb) => {
   // add image to Mongo Image model
+  const newPhoto = new Photo(image);
+  newPhoto.save(err => err || cb());
 };
 
 const editEntry = () => {};
--- server/seedingScript.js
+++ server/seedingScript.js
@@ -1,10 +1,38 @@
-const { createEntry } = require('./database/controllers');
+/* eslint-disable no-plusplus */
+const faker = require('faker');
+const { createEntry } = require('../database/controllers');
+
+const urlOptions = ['burger', 'chicken', 'salad', 'hot-dog', 'pizza'];
 
 const seedingScript = () => {
-  let count = 0;
-  while (count < 10000) {
-    count++;
-  }
+  let currentIndexInUrlOptionsArray = 0;
+  const recursiveGenerator = (currentPid = 0) => {
+    currentIndexInUrlOptionsArray = currentIndexInUrlOptionsArray > 3 ? 0 : ++currentIndexInUrlOptionsArray;
+    const imgUrl = `s3://yelpphotosservice/${urlOptions[currentIndexInUrlOptionsArray]}.jpg`;
+    const uId = faker.random.number({ min: 1, max: 100 });
+    const text = faker.lorem.sentence();
+    const tag = faker.commerce.productName();
+    const bId = faker.random.number({ min: 1, max: 100 });
+
+    createEntry(
+      {
+        currentPid,
+        imgUrl,
+        uId,
+        bId,
+        text,
+        tag,
+      },
+      () => currentPid <= 10000000 && recursiveGenerator(currentPid + 1),
+    );
+    // const { heapTotal, heapUsed, external } = process.memoryUsage();
+    console.log(currentPid, imgUrl, uId, bId, text, tag);
+  };
+  const start = Date.now();
+  recursiveGenerator();
+  const millis = Date.now() - start;
+  console.log(`total seeding time = ${Math.floor(millis / 1000)}`);
 };
+seedingScript();
 
 module.exports = seedingScript;
