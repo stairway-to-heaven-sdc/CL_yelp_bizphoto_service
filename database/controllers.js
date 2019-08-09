6bb5281985da298ac8ae30fe78cb557caeede214
Bruce <bruce@yogainternational.com>
2019-08-09 00:01:49 -0400

    modifies controllers for Postgres integration

--- database/controllers.js
+++ database/controllers.js
@@ -1,16 +1,52 @@
 /* eslint-disable no-console */
 const { Client } = require('pg');
 
-const client = new Client();
-
+const client = new Client({
+  user: process.env.DB_USER,
+  host: 'localhost',
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  database: 'postgres',
+});
 client.connect();
 
-const createEntry = (image) => {
-  client.query('INSERT INTO ');
+console.log('Connected!');
+
+const createEntry = ({
+  pId, imgUrl, uId, bId, text, tag,
+}) => {
+  client.query(
+    `INSERT INTO photos VALUES (
+      ${pId},
+      ${imgUrl},
+      ${uId},
+      ${bId},
+      ${text},
+      ${tag}
+    );`,
+    (err, res) => {
+      console.log(err ? err.stack : 'Success!');
+      client.end();
+    },
+  );
 };
 
 const insertManyIntoDb = (images, cb) => {
-  Photo.collection.insert(images, err => err || cb());
+  client.query(
+    `INSERT INTO photos VALUES
+      ${images.forEach(({
+    pId, imgUrl, uId, bId, text, tag,
+  }, index, arr) => (index !== arr.length - 1
+    ? `(${pId}, ${imgUrl}, ${uId}, ${bId}, ${text}, ${tag}),`
+    : `(${pId}, ${imgUrl}, ${uId}, ${bId}, ${text}, ${tag});`))}
+    `,
+    (err, res) => {
+      console.log(err ? err.stack : 'Success!');
+      // eslint-disable-next-line no-unused-expressions
+      !err && cb();
+      client.end();
+    },
+  );
 };
 
 const editEntry = () => {};
